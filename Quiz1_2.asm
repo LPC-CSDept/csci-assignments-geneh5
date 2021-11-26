1a61cb697d80171c07f9a0145be1392cfad752b6
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-11-25 18:20:31 -0800

    data segment

---
+++ Quiz1_2.asm
@@ -0,0 +1,16 @@
+ #
+ # Quiz1_2.asm
+ #
+ # Gene Han
+ # 10/25
+ # CS21 Quiz 1 Part 2
+ # Evaluating a polynomial
+ #
+
+        .data
+inputa: .asciiz "Input value for variable 'a': "
+inputb: .asciiz "Input value for variable 'b': "
+inputc: .asciiz "Input value for variable 'c': "
+inputx: .asciiz "Input value for variable 'x': "
+done:   .asciiz "ax^2 + bx + c = "
+line:   .asciiz "\n"
