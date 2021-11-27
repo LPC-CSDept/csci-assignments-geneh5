e2e8d1062657e63ce685d617b529e1b2c80e2c81
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-11-26 18:34:52 -0800

    data segment

---
+++ Quiz1_3.asm
@@ -0,0 +1,16 @@
+ #
+ # Quiz1_3.asm
+ #
+ # Gene Han
+ # 10/26
+ # CS21 Quiz 1 Part 3
+ # Newton's Method
+ #
+ 
+        .data
+input:	.asciiz	"Enter value for 'n' variable: "
+done:	.asciiz	"The square root is: "
+line:	.asciiz "\n"
+
+	    .text
+	    .globl main
