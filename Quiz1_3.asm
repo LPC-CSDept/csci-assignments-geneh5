9a36ed5dc8770a8f164a4b986727d9ebab0a2fff
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-11-26 18:40:04 -0800

    input string and loading values

--- Quiz1_3.asm
+++ Quiz1_3.asm
@@ -14,3 +14,15 @@ line:	.asciiz "\n"
 
 	    .text
 	    .globl main
+
+main:
+        la      $a0, input          # load input
+        li      $v0, 4              # print string
+        syscall
+        li      $v0, 6              # read float
+        syscall
+
+        li.s    $f3, 1.0            # first x approx
+        li.s    $f2, 2.0            # load 2.0 const
+        li.s    $f10, 1.0e-5        # terminate condition
+        
