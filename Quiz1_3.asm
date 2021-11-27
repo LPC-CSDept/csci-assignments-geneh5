ea5c06d7332609cf4ff385b99dd84f262b39e7fb
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-11-26 18:50:46 -0800

    loop

--- Quiz1_3.asm
+++ Quiz1_3.asm
@@ -25,4 +25,16 @@ main:
         li.s    $f3, 1.0            # first x approx
         li.s    $f2, 2.0            # load 2.0 const
         li.s    $f10, 1.0e-5        # terminate condition
-        
+
+loop:
+        mov.s	$f4, $f0	        # x' = n
+	    div.s	$f4, $f4, $f3	    # x' = n/x
+	    add.s	$f4, $f3, $f4	    # x' = x + n/x
+	    div.s	$f4, $f4, $f2	    # x' = (1/2)(x + n/x)
+
+	    sub.s	$f5, $f4, $f3	    # x' - x
+	    mov.s	$f3, $f4	        # set new x
+	    abs.s	$f5, $f5	        # abs of x' - x
+	    c.lt.s	$f5, $f10	        # check if less than terminate
+	    bc1f	loop		        # loop
+	    nop
