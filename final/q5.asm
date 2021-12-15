ed0ab982c1ed5b23a465cfafd86c2e17d1d970ac
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-12-14 20:46:54 -0800

    added constants and branch for specific digit

--- final/q5.asm
+++ final/q5.asm
@@ -16,6 +16,10 @@ main:
 
         li      $t0, 3                  # countdown to 0 for loop (number of digits)
         lui     $t1, 0xFFFF             # load receiver control address in kernel
+        li      $t3, 2                  # constant to check which loop
+        li      $t4, 1                  # constant to check which loop
+        li      $t5, 100                # constant to multiply first digit
+        li      $t6, 10                 # constant to multiply second digit
 
 loop:
         lw      $t2, 0($t1)             # load from input control register
@@ -27,4 +31,10 @@ loop:
         sub     $s0, $s0, 48            # subtract ascii code to get digit value
         sub     $t0, $t0, 1             # subtract counter for loop
         beqz    $t0, done               # finished reading 3 digits, branch to done
+        nop                             # delay slot after branch
+
+        beq     $t0, $t3, first         # branch to first on first digit
+        nop                             # delay slot after branch
+
+        beq     $t0, $t4, sec           # branch to sec on second digit
         nop                             # delay slot after branch
