9bfc964e7116f1f420ed154e6ddc1b73dbeffeab
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-12-14 20:08:16 -0800

    added loop counter loaded receiver control address

--- final/q5.asm
+++ final/q5.asm
@@ -10,6 +10,12 @@ input:  .asciiz "Enter digit: "         # input prompt
         .globl main
 
 main:
-        la      $a0, input
-        li      $v0, 4
+        la      $a0, input              # load address of input string
+        li      $v0, 4                  # load print string code
         syscall
+
+        li      $t0, 3                  # countdown to 0 for loop (number of digits)
+        lui     $t1, 0xFFFF             # load receiver control address in kernel
+
+loop:
+        
