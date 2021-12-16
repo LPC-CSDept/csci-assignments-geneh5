e653813cec945aa1b7f3ff96282c29d236b92748
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-12-16 02:17:45 -0800

    created data segment

--- final/q6.asm
+++ final/q6.asm
@@ -4,3 +4,16 @@
 # 12/15
 #
 
+            .kdata
+s1:         .word 10
+s2:         .word 11
+
+            .data
+pr:         .asciiz "Enter any character to output ('q' to quit): "
+line:       .asciiz "\n"
+
+            .text
+            .globl main
+
+main:
+
