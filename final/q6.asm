9ebc9137be563c63e2d4ca95368d70b6ceef940d
Gene Han <gxhan0002@zonemail.clpccd.edu>
2021-12-16 02:22:48 -0800

    loaded prompt and enabled interrupts

--- final/q6.asm
+++ final/q6.asm
@@ -4,16 +4,23 @@
 # 12/15
 #
 
-            .kdata
-s1:         .word 10
-s2:         .word 11
+        .kdata
+s1:     .word 10
+s2:     .word 11
 
-            .data
-pr:         .asciiz "Enter any character to output ('q' to quit): "
-line:       .asciiz "\n"
+        .data
+pr:     .asciiz "Enter any character to output ('q' to quit): "
+line:   .asciiz "\n"
 
-            .text
-            .globl main
+        .text
+        .globl main
 
 main:
 
+        la      $a0,pr              # load address of prompt
+        li      $v0, 4              # code for print string
+        syscall
+
+        mfc0    $a0, $12            # move from c0 (status register)
+        ori     $a0, 0xFF11         # enable all interrupts in $12
+        mtc0    $a0, $12            # move to c0 (status register)
